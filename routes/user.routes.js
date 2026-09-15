e344cebf34a51ae3a8911b59b996b5ecf928fb76
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2026-09-15 18:16:48 +0530

    Add files via upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,1 +1,28 @@
+const express = require("express");
 
+const {
+  getAllUsers,
+  getUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users.controller");
+
+const validateUser = require("../middleware/validate-user.middleware");
+
+const router = express.Router();
+
+router.get("/", getAllUsers);
+
+router.get("/:id", getUserById);
+
+router.get("/test-error", (req, res, next) => {
+  const error = new Error("This is a test error");
+  
+  next(error);
+});
+router.post("/", validateUser, addUser);
+router.put("/:id", validateUser, updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
